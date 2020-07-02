bce281b63001359df817f8d55c2b37abe7e992e6
Omid Rajabi <omid.345@gmail.com>
2020-07-02 17:58:15 -0400

    added sprite and description fields

--- pokedex/app/src/main/java/com/example/pokedex/PokemonActivity.java
+++ pokedex/app/src/main/java/com/example/pokedex/PokemonActivity.java
@@ -1,7 +1,14 @@
 package com.example.pokedex;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,12 +22,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PokemonActivity extends AppCompatActivity {
-    private TextView nameTextView, numberTextView, type1TextView, type2TextView;
+    private TextView nameTextView, numberTextView, type1TextView, type2TextView, descriptionTextView;
     private String url;
+    private Boolean caught = false;
+    private Button catchButton;
+    private ImageView spriteImageView;
     private RequestQueue requestQueue;
 
     @Override
@@ -34,6 +47,9 @@ public class PokemonActivity extends AppCompatActivity {
         numberTextView = findViewById(R.id.pokemon_number);
         type1TextView = findViewById(R.id.pokemon_type1);
         type2TextView = findViewById(R.id.pokemon_type2);
+        descriptionTextView = findViewById(R.id.pokemon_description);
+        catchButton = findViewById(R.id.pokemon_catch);
+        spriteImageView = findViewById(R.id.pokemon_sprite);
 
         load();
     }
@@ -61,6 +77,14 @@ public class PokemonActivity extends AppCompatActivity {
                             type2TextView.setText(type);
                         }
                     }
+
+                    JSONObject spriteUrls = response.getJSONObject("sprites");
+                    String front_default = spriteUrls.getString("front_default");
+                    new DownloadSpriteTask().execute(front_default);
+
+                    String url_description = response.getJSONObject("species").getString("url");
+                    description(url_description);
+
                 } catch (JSONException e) {
                     Log.e("omid", "pokemon json error", e);
                 }
@@ -74,4 +98,69 @@ public class PokemonActivity extends AppCompatActivity {
 
         requestQueue.add(request);
     }
+
+    public void toggleCatch(View v) {
+        if (caught) {
+            caught = false;
+            catchButton.setText("Catch!");
+        } else {
+            caught = true;
+            catchButton.setText("Release!");
+        }
+    }
+
+    /*
+    //TODO: save state
+    getPreferences(Context.MODE_PRIVATE).edit().putString("course", "cs50").commit();
+    String course = getPreferences(Context.MODE_PRIVATE).getString("course", "cs50");
+    //course is equal to "cs50"
+    */
+
+    private class DownloadSpriteTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                return BitmapFactory.decodeStream(url.openStream());
+            } catch (IOException e) {
+                Log.e("omid", "Download sprite error", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            spriteImageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private void description(String url) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response_description) {
+                try {
+                    JSONArray entries = response_description.getJSONArray("flavor_text_entries");
+                    for (int j = 0; j < entries.length(); j++) {
+                        JSONObject entry = entries.getJSONObject(j);
+                        String lang = entry.getJSONObject("language").getString("name");
+                        if (lang.equals("en")) {
+                            descriptionTextView.setText(entry.getString("flavor_text"));
+                            break;
+                        }
+                    }
+                } catch (JSONException e) {
+                    Log.e("omid", "Description json error", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("omid", "pokemon species error", error);
+            }
+        });
+
+        requestQueue.add(request);
+    }
+
 }
